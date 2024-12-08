a0519d9173868ce0e1de9ad390514d509b5ea1d2
Citron <citronovec.kysely@gamil.com>
2024-12-08 20:01:46 +0100

    comim

---
+++ src/main/java/net/citron/pilgrim/PilgrimClient.java
@@ -0,0 +1,10 @@
+package net.citron.pilgrim;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PilgrimClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
